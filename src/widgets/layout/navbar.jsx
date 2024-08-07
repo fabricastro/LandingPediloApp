3412e6e67f8c212d46fe3717dfc3f5f043ae12db
fabricastro <fabricastrook@gmail.com>
2024-08-07 12:56:29 -0300

    first commit

---
+++ src/widgets/layout/navbar.jsx
@@ -0,0 +1,169 @@
+import React from "react";
+import PropTypes from "prop-types";
+import { Link } from "react-router-dom";
+import {
+  Navbar as MTNavbar,
+  MobileNav,
+  Typography,
+  Button,
+  IconButton,
+} from "@material-tailwind/react";
+import { Bars3Icon, XMarkIcon } from "@heroicons/react/24/outline";
+
+export function Navbar({ brandName, routes, action }) {
+  const [openNav, setOpenNav] = React.useState(false);
+
+  React.useEffect(() => {
+    window.addEventListener(
+      "resize",
+      () => window.innerWidth >= 960 && setOpenNav(false)
+    );
+  }, []);
+
+  const navList = (
+    <ul className="mb-4 mt-2 flex flex-col gap-2 text-inherit lg:mb-0 lg:mt-0 lg:flex-row lg:items-center lg:gap-6">
+      {routes.map(({ name, path, icon, href, target }) => (
+        <Typography
+          key={name}
+          as="li"
+          variant="small"
+          color="inherit"
+          className="capitalize"
+        >
+          {href ? (
+            <a
+              href={href}
+              target={target}
+              className="flex items-center gap-1 p-1 font-bold"
+            >
+              {icon &&
+                React.createElement(icon, {
+                  className: "w-[18px] h-[18px] opacity-75 mr-1",
+                })}
+              {name}
+            </a>
+          ) : (
+            <Link
+              to={path}
+              target={target}
+              className="flex items-center gap-1 p-1 font-bold"
+            >
+              {icon &&
+                React.createElement(icon, {
+                  className: "w-[18px] h-[18px] opacity-75 mr-1",
+                })}
+              {name}
+            </Link>
+          )}
+        </Typography>
+      ))}
+    </ul>
+  );
+
+  return (
+    <div className="-m-6 max-h-[768px] w-[calc(100%+48px)] overflow-scroll">
+    <MTNavbar className="sticky top-0 z-10 h-max max-w-full rounded-none px-4 py-2 lg:px-8 lg:py-4">
+      <div className="flex items-center justify-between text-blue-gray-900">
+        <Typography
+          as="a"
+          href="#"
+          className="mr-4 cursor-pointer py-1.5 font-medium"
+        >
+          Material Tailwind
+        </Typography>
+        <div className="flex items-center gap-4">
+          <div className="mr-4 hidden lg:block">{navList}</div>
+          <div className="flex items-center gap-x-1">
+            <Button
+              variant="text"
+              size="sm"
+              className="hidden lg:inline-block"
+            >
+              <span>Log In</span>
+            </Button>
+            <Button
+              variant="gradient"
+              size="sm"
+              className="hidden lg:inline-block"
+            >
+              <span>Sign in</span>
+            </Button>
+          </div>
+          <IconButton
+            variant="text"
+            className="ml-auto h-6 w-6 text-inherit hover:bg-transparent focus:bg-transparent active:bg-transparent lg:hidden"
+            ripple={false}
+            onClick={() => setOpenNav(!openNav)}
+          >
+            {openNav ? (
+              <svg
+                xmlns="http://www.w3.org/2000/svg"
+                fill="none"
+                className="h-6 w-6"
+                viewBox="0 0 24 24"
+                stroke="currentColor"
+                strokeWidth={2}
+              >
+                <path
+                  strokeLinecap="round"
+                  strokeLinejoin="round"
+                  d="M6 18L18 6M6 6l12 12"
+                />
+              </svg>
+            ) : (
+              <svg
+                xmlns="http://www.w3.org/2000/svg"
+                className="h-6 w-6"
+                fill="none"
+                stroke="currentColor"
+                strokeWidth={2}
+              >
+                <path
+                  strokeLinecap="round"
+                  strokeLinejoin="round"
+                  d="M4 6h16M4 12h16M4 18h16"
+                />
+              </svg>
+            )}
+          </IconButton>
+        </div>
+      </div>
+      <MobileNav open={openNav}>
+        {navList}
+        <div className="flex items-center gap-x-1">
+          <Button fullWidth variant="text" size="sm" className="">
+            <span>Log In</span>
+          </Button>
+          <Button fullWidth variant="gradient" size="sm" className="">
+            <span>Sign in</span>
+          </Button>
+        </div>
+      </MobileNav>
+    </MTNavbar>
+    </div>
+  );
+}
+
+Navbar.defaultProps = {
+  brandName: "PediloApp",
+  action: (
+    <a
+      href="https://www.technodevs.com/product/material-tailwind-kit-react"
+      target="_blank"
+    >
+      <Button variant="gradient" size="sm" fullWidth>
+        Probar demo
+      </Button>
+    </a>
+  ),
+};
+
+Navbar.propTypes = {
+  brandName: PropTypes.string,
+  routes: PropTypes.arrayOf(PropTypes.object).isRequired,
+  action: PropTypes.node,
+};
+
+Navbar.displayName = "/src/widgets/layout/navbar.jsx";
+
+export default Navbar;
